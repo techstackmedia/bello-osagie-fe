e9268da6049d678013d04e0a038249122cb35a5a
ts.learndev <ts.learndev@gmail.com>
2024-07-05 01:00:09 +0000

    refactor: refactor navbar component

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 const NavBar = () => {
   return (
-    <nav className="bg-white border-b">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="bg-white border-b ">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
@@ -21,17 +21,21 @@ const NavBar = () => {
             </div>
           </div>
           <div className="flex items-center space-x-4">
-            <button className="text-gray-500 hover:text-gray-700">
-              <Image src="/alert.png" alt='alart' width={24} height={24} />
+            <button className="text-gray-500 hover:text-gray-700 flex flex-col items-center">
+              <Image src="/alert.svg" alt='alart' width={24} height={24} />
+              <span>Notifications</span>
             </button>
-            <button className="text-gray-500 hover:text-gray-700">
-            <Image src="/wallet.png" alt='alart' width={24} height={24} />
+            <button className="text-gray-500 hover:text-gray-700 flex flex-col items-center">
+            <Image src="/wallet.svg" alt='alart' width={24} height={24} />
+            <span>Wallet</span>
             </button>
-            <button className="text-gray-500 hover:text-gray-700">
-            <Image src="/question.png" alt='question' width={24} height={24} />
+            <button className="text-gray-500 hover:text-gray-700 flex flex-col items-center">
+            <Image src="/question.svg" alt='question' width={24} height={24} />
+            <span>Inquiries</span>
             </button>
-            <button className="text-gray-500 hover:text-gray-700">
-            <Image src="/setting.png" alt='question' width={24} height={24} />
+            <button className="text-gray-500 hover:text-gray-700 flex flex-col items-center">
+            <Image src="/setting.svg" alt='question' width={24} height={24} />
+            <span>Settings</span>
             </button>
             <img className="h-8 w-8 rounded-full" src="/profile.png" alt="User" />
             <Image src="/dropdown.svg" alt='question' width={24} height={24}  className='w-[24px] h-[24px]' />
